b83fa01672c6711ae66f44d298166a40ac097eae
Fl0o0o <164840719+Fl0o0o@users.noreply.github.com>
2026-03-10 18:13:44 -0400

    Create basic_if.py.py

---
+++ week8/basic_if.py.py
@@ -0,0 +1,11 @@
+temperature = 30 
+
+if temperature > 25:
+    print("It is hot!")
+    name = input("Enter your name: ")
+
+if name == "Flo":
+    print("Your name is great!")
+else:
+    print("That's a nice name!")
+    
